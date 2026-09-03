9db78b990f86eddecf18a94d34e642c1a54a8084
Michael Okrzesik <mj3.14okrzesik@sbcglobal.net>
2026-09-03 14:25:07 -0500

    Global company constants

---
+++ src/lib/data/site.ts
@@ -0,0 +1,11 @@
+export const site = {
+  companyName: 'Memori',
+  tagline: 'Quality products and personalized service.',
+  email: 'test@email.com',
+  phone: '(555) 555-5555',
+  social: {
+    facebook: 'www.google.com',
+    intagram: 'www.bing.com',
+    etsy: 'www.duckduckgo.com'
+  }
+}
